d6c31f57d509ada42c1b60134e2f039d7a99781e
malashonock <malashonock@gmail.com>
2021-08-21 16:01:34 +0300

    feat: implement min, max, avg methods

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0) {
+    return 0;
+  }
+
+  return array.sort((a, b) => a - b)[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0) {
+    return 0;
+  }
+
+  return array.sort((a, b) => b - a)[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0) {
+    return 0;
+  }
+
+  return array.reduce((previous, current) => previous + current) / array.length;
 }
